a467f122f2332d8e42ab35f939c03d7945409a7f
bertolo1988 <tiagobertolo@gmail.com>
2016-11-03 00:17:57 +0000

    add tests

--- src/MomondoScrappper.ts
+++ src/MomondoScrappper.ts
@@ -3,6 +3,10 @@ import * as Flight from './flight-api/Flight';
 
 class MomondoScrappper {
 
+    static hello = function () {
+        return 'Hello';
+    }
+
     static scrap = function (route: String, date: any, dateFormat: String, currency: String, directFlight: Boolean, timeout: Number, retries: Number) {
 
     }
